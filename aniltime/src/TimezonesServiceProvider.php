1154acf4199c39b639a931894e953c7ec1318f69
anilpavuluri441 <anil1964000@gmail.com>
2018-03-06 17:41:31 +0530

    First commit

---
+++ aniltime/src/TimezonesServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace laraveldaily\aniltime;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views', 'timezones');
+
+         $this->publishes([
+        __DIR__.'/views' => base_path('resources/views/laraveldaily/aniltime'),
+    ]);
+    }
+    public function register()
+    {
+        include __DIR__.'/routes.php';
+        $this->app->make('laraveldaily\aniltime\TimezonesController');
+    }
+
+   
+}
+
+
---
+++ aniltime/src/routes.php
@@ -0,0 +1,3 @@
+<?php
+Route::get('timezones/{timezone}', 
+  'laraveldaily\aniltime\TimezonesController@index');
